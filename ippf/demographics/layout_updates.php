4fde350327fe39a94920f5d8d09a20d5246a4d99
Kevin Yeh <kevinyeh@alum.mit.edu>
2014-04-24 13:10:20 -0400

    rearrange demographics widgets

--- ippf/demographics/layout_updates.php
+++ ippf/demographics/layout_updates.php
@@ -44,8 +44,51 @@
         var buttons_selector=" tr > td > a.css_button_small:first";
         var sections=$(sections);
         sections.find(buttons_selector).each(moveWidgetButton)
-    }    
+    }
+    function position_widgets()
+    {
+        right_column.children("div:first").remove();
+        position_billing();
+        position_patient_reminders();
+        position_contraception();
+    }
+    function find_widget(translated_name)
+    {
+        var heading=$("span.text > b:contains('"+translated_name+"')");
+        var section_heading=heading.parents("div.section-header");
+        var widget=section_heading.parent().parent("tr");
+        return widget;
+    }
+    function prep_left_widget_for_right_side(elem)
+    {
+        var header=elem.find(".section-header").removeClass("section-header").addClass("section-header-dynamic").css("width","100%");
+        var new_div=$("<div></div>");
+        elem.children("td").children().each(function(idx,child)
+        {
+            new_div.append(child);
+        });
+        return new_div;
+    }
+    function position_billing()
+    {
+        var billing_widget=find_widget("<?php echo xl("Billing");?>");
+        right_column.prepend(prep_left_widget_for_right_side(billing_widget));
+    }
+    function position_patient_reminders()
+    {
+        var reminders_widget=prep_left_widget_for_right_side(find_widget("<?php echo xl("Patient Reminders");?>"));
+        right_column.children("div:nth-child(2)").children().eq(1).after(reminders_widget);
+    }
+    function position_contraception()
+    {
+        var demographics=find_widget("<?php echo xl("Demographics");?>");
+        var contraception=find_widget("<?php echo xl("Contraception");?>");
+        demographics.after(contraception);
+    }
+    var columns=$("body > div > table > tbody > tr > td > div");
+    var right_column=columns.eq(1).children("table").children("tbody").children("tr").children("td")
     setup_links();
     positionWidgetButtons("div.section-header");
     positionWidgetButtons("div.section-header-dynamic");
+    position_widgets();
 </script>
