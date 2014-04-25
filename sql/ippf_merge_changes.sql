043dd2bcd641d1660b5f649535ad0c44156850fc
Kevin Yeh <kevinyeh@alum.mit.edu>
2014-04-25 13:58:12 -0400

    IPPF Upgrade SQL changes

--- sql/ippf_merge_changes.sql
+++ sql/ippf_merge_changes.sql
@@ -1,3 +1,5 @@
 #IfMissingColumn procedure_report review_status
 ALTER TABLE `procedure_report` ADD COLUMN `review_status` varchar(31) NOT NULL default '';
 #EndIf
+
+update registry set state=0 where directory in ("vitalsM","vitalsigns");
