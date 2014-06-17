37d71b4c3ce3051048e233e6fff36f43c0d82c9c
Kevin Yeh <kevinyeh@alum.mit.edu>
2014-06-17 12:12:35 -0400

    Run English to English translations as part of dbUpgrade.php. Disable eSign in ippf_merge_changes.sql

--- sql/ippf_merge_changes.sql
+++ sql/ippf_merge_changes.sql
@@ -1,3 +1,5 @@
+update `globals` set gl_value=0 where gl_name in ('esign_individual','lock_esign_individual');
+
 #IfMissingColumn procedure_report review_status
 ALTER TABLE `procedure_report` ADD COLUMN `review_status` varchar(31) NOT NULL default '';
 #EndIf
