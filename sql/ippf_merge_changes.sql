c11f805132d68d17c560df1bbec2aca65f7e8d99
Kevin Yeh <kevinyeh@alum.mit.edu>
2014-07-17 19:09:57 -0400

    Create demographics lists/fields for Nationality, Religion and Education Level

--- sql/ippf_merge_changes.sql
+++ sql/ippf_merge_changes.sql
@@ -4,7 +4,7 @@ update `globals` set gl_value=0 where gl_name in ('esign_individual','lock_esign
 ALTER TABLE `procedure_report` ADD COLUMN `review_status` varchar(31) NOT NULL default '';
 #EndIf
 
-update registry set state=0 where directory in ("vitalsM","vitalsigns");
+update registry set state=0 where directory in ("vitalsM","vitalsigns","misc_billing_options","dictation");
 
 #IfMissingColumn procedure_order procedure_type_id
 ALTER TABLE `procedure_order` 
@@ -12,7 +12,36 @@ ADD COLUMN `procedure_type_id` BIGINT(20) NOT NULL COMMENT "references procedure
 #EndIf
 
 
+UPDATE list_options SET title = 'User Defined List 2' WHERE list_id = 'lists' AND option_id = 'userlist2';
+
 #IfMissingColumn procedure_result procedure_type_id
 ALTER TABLE `procedure_result` 
 ADD COLUMN `procedure_type_id` BIGINT(20) NOT NULL COMMENT "references procedure_type.procedure_type_id";
+#EndIf
+
+#IfMissingColumn patient_data education_level
+ALTER TABLE `patient_data` ADD COLUMN `education_level` varchar(10) NOT NULL default '';
+INSERT INTO `layout_options` (`form_id`,`field_id`,`group_name`,`title`,`seq`,`data_type`,`uor`,`fld_length`,`max_length`,`list_id`,`titlecols`,`datacols`,`default_value`,`edit_options`,`description`,`fld_rows`) VALUES ('DEM', 'education_level', '6Misc', 'Education Level',20, 1, 1, 0, 0, 'education_level', 1, 1, '', '', 'Education Level', 0);
+INSERT INTO list_options ( list_id, option_id, title, seq, is_default, mapping ) VALUES ('lists','education_level','Education Level',1,0,'0');
+INSERT INTO list_options ( list_id, option_id, title, seq, is_default, mapping ) VALUES ('education_level','1','Illiterate',1,0,'0');
+INSERT INTO list_options ( list_id, option_id, title, seq, is_default, mapping ) VALUES ('education_level','2','Basic Schooling',2,0,'1');
+INSERT INTO list_options ( list_id, option_id, title, seq, is_default, mapping ) VALUES ('education_level','3','Advanced Schooling',3,0,'2');
+#EndIf
+
+
+#IfMissingColumn patient_data religion
+ALTER TABLE `patient_data` ADD COLUMN `religion` varchar(10) NOT NULL default '';
+INSERT INTO `layout_options` (`form_id`,`field_id`,`group_name`,`title`,`seq`,`data_type`,`uor`,`fld_length`,`max_length`,`list_id`,`titlecols`,`datacols`,`default_value`,`edit_options`,`description`,`fld_rows`) VALUES ('DEM', 'religion', '6Misc', 'Religion',19, 1, 1, 0, 0, 'religion', 1, 1, '', '', 'Religion', 0);
+INSERT INTO list_options ( list_id, option_id, title, seq, is_default, mapping ) VALUES ('lists','religion','Religion',1,0,'0');
+INSERT INTO list_options ( list_id, option_id, title, seq, is_default, mapping ) VALUES ('religion','1','Catholic',1,0,'0');
+INSERT INTO list_options ( list_id, option_id, title, seq, is_default, mapping ) VALUES ('religion','2','Other',2,0,'1');
+#EndIf
+
+
+#IfMissingColumn patient_data nationality
+ALTER TABLE `patient_data` ADD COLUMN `nationality` varchar(10) NOT NULL default '';
+INSERT INTO `layout_options` (`form_id`,`field_id`,`group_name`,`title`,`seq`,`data_type`,`uor`,`fld_length`,`max_length`,`list_id`,`titlecols`,`datacols`,`default_value`,`edit_options`,`description`,`fld_rows`) VALUES ('DEM', 'nationality', '6Misc', 'Nationality',21, 1, 1, 0, 0, 'nationality', 1, 1, '', '', 'Nationality', 0);
+INSERT INTO list_options ( list_id, option_id, title, seq, is_default, mapping ) VALUES ('lists','nationality','Nationality',1,0,'0');
+INSERT INTO list_options ( list_id, option_id, title, seq, is_default, mapping ) VALUES ('nationality','1','Sample 1',1,0,'0');
+INSERT INTO list_options ( list_id, option_id, title, seq, is_default, mapping ) VALUES ('nationality','2','Sample 2',2,0,'1');
 #EndIf
