70c4db670fd3e72b0bd1cc1fed97d53e2004f585
Kevin Yeh <kevinyeh@alum.mit.edu>
2014-08-04 17:59:24 -0400

    Set facility name in form_encounter when null during upgrade

--- sql/ippf_3_3_0-to-4_1_3_upgrade.sql
+++ sql/ippf_3_3_0-to-4_1_3_upgrade.sql
@@ -1,1 +1,3 @@
 update layout_options SET uor=0 where `field_id` in ('education_level','religion','nationality');
+
+update form_encounter set facility = (select name from facility where facility.id=form_encounter.facility_id) where facility is null;
