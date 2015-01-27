3cd0b1820c3f1a6a10ed2b96857cdca2cc1c0544
Rod Roark <rod@sunsetsystems.com>
2015-01-27 15:07:13 -0800

    Added 2 new columns, column sorting and row colors to the Destroyed Drugs Report.

--- interface/reports/destroyed_drugs_report.php
+++ interface/reports/destroyed_drugs_report.php
@@ -14,11 +14,22 @@
  require_once("../drugs/drugs.inc.php");
  require_once("$srcdir/formatting.inc.php");
 
- $form_from_date  = fixDate($_POST['form_from_date'], date('Y-01-01'));
- $form_to_date    = fixDate($_POST['form_to_date']  , date('Y-m-d'));
+// For each sorting option, specify the ORDER BY argument.
+$ORDERHASH = array(
+  'drug' => 'd.name, i.drug_id, i.destroy_date, i.lot_number',
+  'fac'  => 'facility, i.destroy_date, d.name, i.drug_id, i.lot_number',
+  'wh'   => 'warehouse, i.destroy_date, d.name, i.drug_id, i.lot_number',
+  'date' => 'i.destroy_date, d.name, i.drug_id, i.lot_number',
+);
 
- // The selected facility ID, if any.
- $form_facility = 0 + empty($_REQUEST['form_facility']) ? 0 : $_REQUEST['form_facility'];
+$form_from_date  = fixDate($_POST['form_from_date'], date('Y-01-01'));
+$form_to_date    = fixDate($_POST['form_to_date']  , date('Y-m-d'));
+
+// The selected facility ID, if any.
+$form_facility = 0 + empty($_REQUEST['form_facility']) ? 0 : $_REQUEST['form_facility'];
+
+$form_orderby = $ORDERHASH[$_REQUEST['form_orderby']] ? $_REQUEST['form_orderby'] : 'drug';
+$orderby = $ORDERHASH[$form_orderby];
 ?>
 <html>
 <head>
@@ -32,9 +43,19 @@
 <script type="text/javascript" src="../../library/dynarch_calendar_en.js"></script>
 <script type="text/javascript" src="../../library/dynarch_calendar_setup.js"></script>
 <script type="text/javascript" src="../../library/dialog.js"></script>
+
 <script language="JavaScript">
  var mypcc = '<?php echo $GLOBALS['phone_country_code'] ?>';
+
+ function dosort(orderby) {
+  var f = document.forms[0];
+  f.form_orderby.value = orderby;
+  top.restoreSession();
+  f.submit();
+  return false;
+ }
 </script>
+
 </head>
 
 <body leftmargin='0' topmargin='0' marginwidth='0' marginheight='0'>
@@ -98,32 +119,46 @@ echo "   </select>&nbsp;\n";
 <table border='0' cellpadding='1' cellspacing='2' width='98%'>
  <tr bgcolor="#dddddd">
   <td class='dehead'>
-   <?php xl('Drug Name','e'); ?>
+   <a href="#" onclick="return dosort('drug')"
+   <?php if ($form_orderby == "drug") echo " style=\"color:#00cc00\""; ?>>
+   <?php echo xlt('Drug Name'); ?> </a>
   </td>
   <td class='dehead'>
-   <?php xl('NDC','e'); ?>
+   <?php echo xlt('NDC'); ?>
   </td>
   <td class='dehead'>
-   <?php xl('Lot','e'); ?>
+   <?php echo xlt('Lot'); ?>
   </td>
   <td class='dehead'>
-   <?php xl('Qty','e'); ?>
+   <a href="#" onclick="return dosort('fac')"
+   <?php if ($form_orderby == "fac") echo " style=\"color:#00cc00\""; ?>>
+   <?php echo xlt('Facility'); ?> </a>
   </td>
   <td class='dehead'>
-   <?php xl('Date Destroyed','e'); ?>
+   <a href="#" onclick="return dosort('wh')"
+   <?php if ($form_orderby == "wh") echo " style=\"color:#00cc00\""; ?>>
+   <?php echo xlt('Warehouse'); ?> </a>
   </td>
   <td class='dehead'>
-   <?php xl('Method','e'); ?>
+   <?php echo xlt('Qty'); ?>
   </td>
   <td class='dehead'>
-   <?php xl('Witness','e'); ?>
+   <a href="#" onclick="return dosort('date')"
+   <?php if ($form_orderby == "date") echo " style=\"color:#00cc00\""; ?>>
+   <?php echo xlt('Date Destroyed'); ?> </a>
   </td>
   <td class='dehead'>
-   <?php xl('Notes','e'); ?>
+   <?php echo xlt('Method'); ?>
+  </td>
+  <td class='dehead'>
+   <?php echo xlt('Witness'); ?>
+  </td>
+  <td class='dehead'>
+   <?php echo xlt('Notes'); ?>
   </td>
  </tr>
 <?
- if ($_POST['form_refresh']) {
+ if ($_POST['form_orderby']) {
   $where = "i.destroy_date IS NOT NULL AND i.destroy_date >= '$form_from_date' AND " .
    "i.destroy_date <= '$form_to_date'";
 
@@ -133,53 +168,62 @@ echo "   </select>&nbsp;\n";
 
   $query = "SELECT i.inventory_id, i.lot_number, i.on_hand, i.drug_id, " .
    "i.destroy_date, i.destroy_method, i.destroy_witness, i.destroy_notes, " .
-   "d.name, d.ndc_number " .
+   "d.name, d.ndc_number, lo.title AS warehouse, f.name AS facility " .
    "FROM drug_inventory AS i " .
-   "LEFT OUTER JOIN drugs AS d ON d.drug_id = i.drug_id " .
+   "LEFT JOIN drugs AS d ON d.drug_id = i.drug_id " .
    "LEFT JOIN list_options AS lo ON lo.list_id = 'warehouse' AND " .
    "lo.option_id = i.warehouse_id " .
+   "LEFT JOIN facility AS f ON f.id = lo.option_value " .
    "WHERE $where " .
-   "ORDER BY d.name, i.drug_id, i.destroy_date, i.lot_number";
+   "ORDER BY $orderby";
 
   // echo "<!-- $query -->\n"; // debugging
   $res = sqlStatement($query);
 
   $last_drug_id = 0;
+  $encount = 0;
   while ($row = sqlFetchArray($res)) {
-   $drug_name       = $row['name'];
-   $ndc_number      = $row['ndc_number'];
+   $drug_name       = text($row['name']);
+   $ndc_number      = text($row['ndc_number']);
    if ($row['drug_id'] == $last_drug_id) {
     $drug_name  = '&nbsp;';
     $ndc_number = '&nbsp;';
    }
+   $bgcolor = (++$encount & 1) ? "#ddddff" : "#ffdddd";
 ?>
- <tr>
+ <tr bgcolor="<?php echo $bgcolor; ?>">
   <td class='detail'>
-   <?php echo $drug_name ?>
+   <?php echo $drug_name; ?>
   </td>
   <td class='detail'>
-   <?php echo $ndc_number ?>
+   <?php echo $ndc_number; ?>
   </td>
   <td class='detail'>
    <a href='../drugs/destroy_lot.php?drug=<?php echo $row['drug_id'] ?>&lot=<?php echo $row['inventory_id'] ?>'
     style='color:#0000ff' target='_blank'>
-   <?php echo $row['lot_number'] ?>
+   <?php echo text($row['lot_number']); ?>
    </a>
   </td>
   <td class='detail'>
-   <?php echo $row['on_hand'] ?>
+   <?php echo text($row['facility']); ?>
+  </td>
+  <td class='detail'>
+   <?php echo text($row['warehouse']); ?>
+  </td>
+  <td class='detail'>
+   <?php echo text($row['on_hand']); ?>
   </td>
   <td class='detail'>
    <?php echo oeFormatShortDate($row['destroy_date']) ?>
   </td>
   <td class='detail'>
-   <?php echo $row['destroy_method'] ?>
+   <?php echo text($row['destroy_method']); ?>
   </td>
   <td class='detail'>
-   <?php echo $row['destroy_witness'] ?>
+   <?php echo text($row['destroy_witness']); ?>
   </td>
   <td class='detail'>
-   <?php echo $row['destroy_notes'] ?>
+   <?php echo text($row['destroy_notes']); ?>
   </td>
  </tr>
 <?php
@@ -189,6 +233,9 @@ echo "   </select>&nbsp;\n";
 ?>
 
 </table>
+
+<input type="hidden" name="form_orderby" value="<?php echo $form_orderby ?>" />
+
 </form>
 </center>
 <script language='JavaScript'>
