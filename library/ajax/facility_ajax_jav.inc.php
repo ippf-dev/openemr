3930bf14719017c988feac89fb5994da2b75caec
Rod Roark <rod@sunsetsystems.com>
2015-01-29 16:13:25 -0800

    Fixed bug of resetting billing facility in New Encounter form when the service facility is changed. Also fixed some ugly non-use of white space in the code.

--- library/ajax/facility_ajax_jav.inc.php
+++ library/ajax/facility_ajax_jav.inc.php
@@ -30,23 +30,22 @@
 ?>
 <script type="text/javascript">
 function ajax_bill_loc(pid,date,facility){
-top.restoreSession();
-$.ajax({
-type: "POST",
-url: "../../../library/ajax/facility_ajax_code.php",
-dataType: "html",
-data: {
-pid: pid,
-date: date,
-facility: facility
-},
-success: function(thedata){//alert(thedata)
-$("#ajaxdiv").html(thedata);
-},
-error:function(){
-}
-});
-return;
-
+ top.restoreSession();
+ $.ajax({
+  type: "POST",
+  url: "../../../library/ajax/facility_ajax_code.php",
+  dataType: "html",
+  data: {
+   pid: pid,
+   date: date,
+   facility: facility
+  },
+  success: function(thedata) { //alert(thedata)
+   if (thedata.length > 5) $("#ajaxdiv").html(thedata);
+  },
+  error: function(){
+  }
+ });
+ return;
 }
 </script>
