aab0fed5b9ce6e81d4b689f5f85b8081a842eba6
Rod Roark <rod@sunsetsystems.com>
2016-03-15 14:18:06 -0700

    Fix for Services by Category report.

--- interface/reports/services_by_category.php
+++ interface/reports/services_by_category.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (C) 2008-2014 Rod Roark <rod@sunsetsystems.com>
+// Copyright (C) 2008-2016 Rod Roark <rod@sunsetsystems.com>
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -232,7 +232,7 @@ if ($_POST['form_submit'] || $_POST['form_csvexport']) {
     // Get codes that relate to this one.
     $tmp = $key . ':' . $row['code'];
     $relres = sqlStatement("SELECT code_type, code, code_text FROM codes WHERE " .
-      "(related_code LIKE '$tmp' OR related_code LIKE '$tmp;%' OR related_code LIKE '%;$tmp;%') " .
+      "(related_code LIKE '$tmp' OR related_code LIKE '$tmp;%' OR related_code LIKE '%;$tmp' OR related_code LIKE '%;$tmp;%') " .
       "AND active = 1 ORDER BY code_type, code");
     while ($relrow = sqlFetchArray($relres)) {
       $reltype = '?';
