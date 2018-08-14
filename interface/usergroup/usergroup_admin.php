8b515858b97bcd25ccd8fe92bb8cfaa729587179
Rod Roark <rod@sunsetsystems.com>
2018-08-15 09:28:18 +1000

    Adapted from Security Fixes 26 on 2015-12-13.

--- interface/usergroup/usergroup_admin.php
+++ interface/usergroup/usergroup_admin.php
@@ -73,8 +73,9 @@ function setUserFacilities($user_id) {
         "tablename = ? AND table_id = ? AND facility_id = ? AND warehouse_id = ?",
         array('users', $user_id, $facid, $whid));
         ********************************************************/
-        "tablename = 'users' AND table_id = " . $user_id .
-        " AND facility_id = '$facid' AND warehouse_id = '$whid'");
+        "tablename = 'users' AND table_id = " . add_escape_custom($user_id) .
+        " AND facility_id = '" . add_escape_custom($facid) . "' AND warehouse_id = '" .
+        add_escape_custom($whid) . "'");
     }
   }
 }
@@ -87,7 +88,7 @@ $mail_id = explode(".",$SMTP_HOST);
 for($i=0;$i<$bg_count;$i++){
 if(($_GET['access_group'][$i] == "Emergency Login") && ($_GET['active'] == 'on') && ($_GET['pre_active'] == 0)){
   if(($_GET['get_admin_id'] == 1) && ($_GET['admin_id'] != "")){
-	$res = sqlStatement("select username from users where id={$_GET["id"]}");
+	$res = sqlStatement("select username from users where id = ?", array($_GET["id"]));
 	$row = sqlFetchArray($res);
 	$uname=$row['username'];
 	$mail = new MyMailer();
@@ -110,10 +111,9 @@ if (isset($_POST["privatemode"]) && $_POST["privatemode"] =="user_admin") {
       if (isset($_POST["rumple"])) {
         // $tqvar = addslashes(trim($_POST["username"]));
         $tqvar = trim(formData('rumple','P'));
-        $user_data = sqlQuery("select * from users where id={$_POST["id"]}");
-        sqlStatement("update users set username='$tqvar' where id={$_POST["id"]}");
-        sqlStatement("update groups set user='$tqvar' where user='". $user_data["username"]  ."'");
-        //echo "query was: " ."update groups set user='$tqvar' where user='". $user_data["username"]  ."'" ;
+        $user_data = sqlQuery("select * from users where id = ?", array($_POST["id"]));
+        sqlStatement("update users set username = '$tqvar' where id = ?", array($_POST["id"]));
+        sqlStatement("update groups set user = '$tqvar' where user = ?", array($user_data["username"]));
       }
       if ($_POST["taxid"]) {
         $tqvar = formData('taxid','P');
@@ -121,41 +121,41 @@ if (isset($_POST["privatemode"]) && $_POST["privatemode"] =="user_admin") {
       }
       if ($_POST["state_license_number"]) {
         $tqvar = formData('state_license_number','P');
-        sqlStatement("update users set state_license_number='$tqvar' where id={$_POST["id"]}");
+        sqlStatement("update users set state_license_number = '$tqvar' where id = ?", array($_POST["id"]));
       }
       if ($_POST["drugid"]) {
         $tqvar = formData('drugid','P');
-        sqlStatement("update users set federaldrugid='$tqvar' where id={$_POST["id"]}");
+        sqlStatement("update users set federaldrugid = '$tqvar' where id = ?", array($_POST["id"]));
       }
       if ($_POST["upin"]) {
         $tqvar = formData('upin','P');
-        sqlStatement("update users set upin='$tqvar' where id={$_POST["id"]}");
+        sqlStatement("update users set upin = '$tqvar' where id = ?", array($_POST["id"]));
       }
       if ($_POST["npi"]) {
         $tqvar = formData('npi','P');
-        sqlStatement("update users set npi='$tqvar' where id={$_POST["id"]}");
+        sqlStatement("update users set npi = '$tqvar' where id = ?", array($_POST["id"]));
       }
       if ($_POST["taxonomy"]) {
         $tqvar = formData('taxonomy','P');
-        sqlStatement("update users set taxonomy = '$tqvar' where id= {$_POST["id"]}");
+        sqlStatement("update users set taxonomy = '$tqvar' where id = ?", array($_POST["id"]));
       }
       if ($_POST["lname"]) {
         $tqvar = formData('lname','P');
-        sqlStatement("update users set lname='$tqvar' where id={$_POST["id"]}");
+        sqlStatement("update users set lname = '$tqvar' where id = ?", array($_POST["id"]));
       }
       if ($_POST["job"]) {
         $tqvar = formData('job','P');
-        sqlStatement("update users set specialty='$tqvar' where id={$_POST["id"]}");
+        sqlStatement("update users set specialty = '$tqvar' where id = ?", array($_POST["id"]));
       }
       if ($_POST["mname"]) {
               $tqvar = formData('mname','P');
-              sqlStatement("update users set mname='$tqvar' where id={$_POST["id"]}");
+              sqlStatement("update users set mname = '$tqvar' where id = ?", array($_POST["id"]));
       }
       if ($_POST["facility_id"]) {
               $tqvar = formData('facility_id','P');
-              sqlStatement("update users set facility_id = '$tqvar' where id = {$_POST["id"]}");
+              sqlStatement("update users set facility_id = '$tqvar' where id = ?", array($_POST["id"]));
               //(CHEMED) Update facility name when changing the id
-              sqlStatement("UPDATE users, facility SET users.facility = facility.name WHERE facility.id = '$tqvar' AND users.id = {$_POST["id"]}");
+              sqlStatement("UPDATE users, facility SET users.facility = facility.name WHERE facility.id = '$tqvar' AND users.id = ?", array($_POST["id"]));
               //END (CHEMED)
       }
 
@@ -163,13 +163,13 @@ if (isset($_POST["privatemode"]) && $_POST["privatemode"] =="user_admin") {
 
       if ($_POST["fname"]) {
               $tqvar = formData('fname','P');
-              sqlStatement("update users set fname='$tqvar' where id={$_POST["id"]}");
+              sqlStatement("update users set fname='$tqvar' where id = ?", array($_POST["id"]));
       }
 
       //(CHEMED) Calendar UI preference
       if ($_POST["cal_ui"]) {
         $tqvar = formData('cal_ui','P');
-        sqlStatement("update users set cal_ui = '$tqvar' where id = {$_POST["id"]}");
+        sqlStatement("update users set cal_ui = '$tqvar' where id = ?", array($_POST["id"]));
 
         // added by bgm to set this session variable if the current user has edited
         //   their own settings
@@ -207,7 +207,7 @@ if (isset($_POST["privatemode"]) && $_POST["privatemode"] =="user_admin") {
       // for relay health single sign-on
       if (isset($_POST["ssi_relayhealth"]) && $_POST["ssi_relayhealth"]) {
         $tqvar = formData('ssi_relayhealth','P');
-        sqlStatement("update users set ssi_relayhealth = '$tqvar' where id = {$_POST["id"]}");
+        sqlStatement("update users set ssi_relayhealth = '$tqvar' where id = ?", array($_POST["id"]));
       }
 
       $tqvar  = $_POST["authorized"] ? 1 : 0;
@@ -216,7 +216,7 @@ if (isset($_POST["privatemode"]) && $_POST["privatemode"] =="user_admin") {
   
       sqlStatement("UPDATE users SET authorized = $tqvar, active = $actvar, " .
         "calendar = $calvar, see_auth = '" . $_POST['see_auth'] . "' WHERE " .
-        "id = {$_POST["id"]}");
+        "id = ?", array($_POST["id"]));
       //Display message when Emergency Login user was activated 
       $bg_count=count($_POST['access_group']);
       for($i=0;$i<$bg_count;$i++){
@@ -233,10 +233,10 @@ if (isset($_POST["privatemode"]) && $_POST["privatemode"] =="user_admin") {
       }	
       if ($_POST["comments"]) {
         $tqvar = formData('comments','P');
-        sqlStatement("update users set info = '$tqvar' where id = {$_POST["id"]}");
+        sqlStatement("update users set info = '$tqvar' where id = ?", array($_POST["id"]));
       }
       $erxrole = formData('erxrole','P');
-      sqlStatement("update users set newcrop_user_role = '$erxrole' where id = {$_POST["id"]}");
+      sqlStatement("update users set newcrop_user_role = '$erxrole' where id = ?", array($_POST["id"]));
 
       if (isset($phpgacl_location) && acl_check('admin', 'acl')) {
         // Set the access control group of user
@@ -379,19 +379,18 @@ if (isset($_GET["mode"])) {
   *******************************************************************/
 
   if ($_GET["mode"] == "delete_group") {
-    $res = sqlStatement("select distinct user from groups where id = '" .
-      $_GET["id"] . "'");
+    $res = sqlStatement("select distinct user from groups where id = ?", array($_GET["id"]));
     for ($iter = 0; $row = sqlFetchArray($res); $iter++)
       $result[$iter] = $row;
     foreach($result as $iter)
       $un = $iter{"user"};
-    $res = sqlStatement("select name, user from groups where user = '$un' " .
-      "and id != '" . $_GET["id"] . "'");
+    $res = sqlStatement("select name, user from groups where user = ? " .
+      "and id != ?", array($un, $_GET["id"]));
 
     // Remove the user only if they are also in some other group.  I.e. every
     // user must be a member of at least one group.
     if (sqlFetchArray($res) != FALSE) {
-      sqlStatement("delete from groups where id = '" . $_GET["id"] . "'");
+      sqlStatement("delete from groups where id = ?", array($_GET["id"]));
     } else {
       $alertmsg .= "You must add this user to some other group before " .
         "removing them from this group. ";
@@ -479,11 +478,13 @@ function dosort(orderby) {
 </form>
 <?php
 if($set_active_msg == 1){
-echo "<font class='alert'>".xl('Emergency Login ACL is chosen. The user is still in active state, please de-activate the user and activate the same when required during emergency situations. Visit Administration->Users for activation or de-activation.')."</font><br>";
+echo "<font class='alert'>".xlt('Emergency Login ACL is chosen. The user is still in active state, please de-activate the user and activate the same when required during emergency situations. Visit Administration->Users for activation or de-activation.')."</font><br>";
 }
 if ($show_message == 1){
- echo "<font class='alert'>".xl('The following Emergency Login User is activated:')." "."<b>".$_GET['fname']."</b>"."</font><br>";
- echo "<font class='alert'>".xl('Emergency Login activation email will be circulated only if following settings in the interface/globals.php file are configured:')." \$GLOBALS['Emergency_Login_email'], \$GLOBALS['Emergency_Login_email_id']</font>";
+ echo "<font class='alert'>" . xlt('The following Emergency Login User is activated:') . " <b>" . text($_GET['fname']) . "</b></font><br>";
+ echo "<font class='alert'>" .
+  xlt('Emergency Login activation email will be circulated only if following settings in the interface/globals.php file are configured:') .
+  text(" \$GLOBALS['Emergency_Login_email'], \$GLOBALS['Emergency_Login_email_id']") . "</font>";
 }
 function create_sortable_header($text,$field,$width)
 {
